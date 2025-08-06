b47a23484efc586fb63fe18fd4ed361fd438a761
alex.lin@datarget.com <alex.lin@datarget.com>
2025-08-06 08:21:07 +0000

    feat: first modeling

---
+++ models/staging/stg_menu.sql
@@ -0,0 +1,18 @@
+-- models/staging/stg_menu.sql
+WITH base AS (
+  SELECT
+    menu_id,
+    menu_type_id,
+    TRIM(menu_type) AS menu_type,
+    TRIM(truck_brand_name) AS truck_brand_name,
+    menu_item_id,
+    TRIM(menu_item_name) AS menu_item_name,
+    item_category,
+    item_subcategory,
+    cost_of_goods_usd,
+    sale_price_usd,
+    menu_item_health_metrics_obj
+  FROM {{ source('snowflake_learning_db', 'menu') }}
+)
+
+SELECT * FROM base;
